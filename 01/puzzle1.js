6536cdbafe70ad98e89a1da89ecf21fc2b0f56d3
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-01 15:01:42 -0800

    Created README and small code cleanup in puzzle1.js.

--- 01/puzzle1.js
+++ 01/puzzle1.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 let sum = 0;
 
-fs.readFile('./day1.txt', 'utf-8', (err, captcha) => {
+fs.readFile('./puzzle1.txt', 'utf-8', (err, captcha) => {
   for (var i = 0; i < captcha.length; i++) {
-    if (captcha[i] === captcha[(i + 1)]) {
-      sum = sum + parseInt(captcha[i], 10);
+    if (captcha[i] === captcha[i + 1]) {
+      sum += parseInt(captcha[i], 10);
     }
   }
 
   if (captcha[captcha.length - 1] === captcha[0]) {
-    sum = sum + parseInt(captcha[0], 10);
+    sum += parseInt(captcha[0], 10);
   }
 
   console.log(sum);
