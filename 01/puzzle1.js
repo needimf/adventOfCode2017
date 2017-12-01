142da2bdb0f15a02823a3361007fb27ed8e1d23d
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-01 14:52:09 -0800

    Solved day1 puzzle1.

---
+++ 01/puzzle1.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+let sum = 0;
+
+fs.readFile('./day1.txt', 'utf-8', (err, captcha) => {
+  for (var i = 0; i < captcha.length; i++) {
+    if (captcha[i] === captcha[(i + 1)]) {
+      sum = sum + parseInt(captcha[i], 10);
+    }
+  }
+
+  if (captcha[captcha.length - 1] === captcha[0]) {
+    sum = sum + parseInt(captcha[0], 10);
+  }
+
+  console.log(sum);
+});
