7e73b20d80181d0f4bd989f3b30c76fc88fff3c5
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-05 17:05:53 -0800

    Solved day 5 puzzle 2.

---
+++ 05/puzzle2.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+fs.readFile('./day5.txt', 'utf-8', (err, data) => {
+  let puzzleInput = data.split('\n').map(number => parseInt(number, 10))
+  let currentIdx = 0
+  let finalIdx = puzzleInput.length - 1
+  let jumps = 0
+
+  while (currentIdx <= finalIdx) {
+    let jumpDistance = puzzleInput[currentIdx]
+    jumpDistance >= 3 ? puzzleInput[currentIdx] -= 1 : puzzleInput[currentIdx] += 1
+    currentIdx += jumpDistance
+    jumps += 1
+  }
+  console.log(jumps)
+})
