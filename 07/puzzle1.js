a8c9bb1c3668d978ae4e5da604a6bf59fd4995b0
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-07 14:21:02 -0800

    Started day 7, and solved day 7 puzzle 1.

---
+++ 07/puzzle1.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+fs.readFile('./day7.txt', 'utf-8', (err, data) => {
+  let puzzleInputPrograms = data.split('\n')
+  let towerTops = puzzleInputPrograms.filter(program => !program.includes('->'))
+  let programsWithDiscs = puzzleInputPrograms.filter(program => program.includes('->'))
+
+  let bottomProgram = false;
+  let bottomProgramName;
+  let index = 0
+  
+  while (bottomProgram === false && index < programsWithDiscs.length) {
+    let programName = programsWithDiscs[index].slice(0, programsWithDiscs[index].indexOf(' '))
+    bottomProgram = programsWithDiscs.every((otherProgram, idx) => {
+      if (index === idx) {
+        return true
+      } else if (!otherProgram.includes(programName)) {
+        return true
+      }
+    })
+    if (bottomProgram) bottomProgramName = programName
+    index++
+  }
+  
+  console.log(bottomProgramName)
+})
