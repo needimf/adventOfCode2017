725664edcf552d9b01f2fd9320faa2c1f3a95c14
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-08 09:33:23 -0800

    Solved day 8 puzzle 2.

---
+++ 08/puzzle2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+fs.readFile('./day8.txt', 'utf-8', (err, data) => {
+  let instructions = data.split('\n')
+  let registers = {}
+  instructions.forEach(instruction => {
+    registers[instruction.slice(0, instruction.indexOf(' '))] = 0
+  })
+
+  let maxRegisterValue = 0;
+
+  instructions.forEach(instruction => {
+    let firstSpace = instruction.indexOf(' ')
+    let register = instruction.slice(0, firstSpace)
+    let operation = instruction.slice(firstSpace + 1, firstSpace + 4)
+    let amount = parseInt(instruction.slice(firstSpace + 5, instruction.indexOf('if') - 1), 10)
+    let condition = instruction.slice(instruction.indexOf('if') + 3)
+    let conditionTarget = condition.slice(0, condition.indexOf(' '))
+    condition = condition.replace(conditionTarget, `registers.${conditionTarget}`)
+    if (eval(condition)) {
+      operation === 'inc' ? registers[register] += amount : registers[register] -= amount
+    }
+    for (var key in registers) {
+      if (registers[key] > maxRegisterValue) {maxRegisterValue = registers[key]}
+    }
+  })
+
+  console.log(maxRegisterValue)
+})
