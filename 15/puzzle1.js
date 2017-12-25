8c95d4e90345dc90b86831030ee8355e19d28f8c
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-24 19:54:46 -0800

    Solved day15 puzzle2, and improved puzzle1 solution.

--- 15/puzzle1.js
+++ 15/puzzle1.js
@@ -8,8 +8,8 @@ let divisor = 2147483647
 let judgeCount = 0
 
 for (let i = 1; i <= 40000000; i++) {
-  let [valueA, valueB] = createNextValues()
-  if ((valueA & 0xFFFF) === (valueB & 0xFFFF)) {
+  createNextValues()
+  if ((generatorAPreviousValue & 0xFFFF) === (generatorBPreviousValue & 0xFFFF)) {
     judgeCount += 1
   }
 }
@@ -17,9 +17,6 @@ for (let i = 1; i <= 40000000; i++) {
 console.log(judgeCount)
 
 function createNextValues() {
-  let valueA = (generatorAPreviousValue * generatorAMultiplier) % divisor
-  let valueB = (generatorBPreviousValue * generatorBMultiplier) % divisor
-  generatorAPreviousValue = valueA
-  generatorBPreviousValue = valueB
-  return [valueA, valueB]
+  generatorAPreviousValue = (generatorAPreviousValue * generatorAMultiplier) % divisor
+  generatorBPreviousValue = (generatorBPreviousValue * generatorBMultiplier) % divisor
 }
--- 15/puzzle2.js
+++ 15/puzzle2.js
@@ -7,3 +7,16 @@ let divisor = 2147483647
 
 let judgeCount = 0
 
+for (let i = 1; i <= 5000000; i++) {
+  createNextValues()
+  if ((generatorAPreviousValue & 0xFFFF) === (generatorBPreviousValue & 0xFFFF)) {
+    judgeCount += 1
+  }
+}
+
+console.log(judgeCount)
+
+function createNextValues() {
+  do { generatorAPreviousValue = (generatorAPreviousValue * generatorAMultiplier) % divisor } while (generatorAPreviousValue % 4 != 0)
+  do { generatorBPreviousValue = (generatorBPreviousValue * generatorBMultiplier) % divisor } while (generatorBPreviousValue % 8 != 0)
+}
